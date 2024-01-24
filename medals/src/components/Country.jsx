885a006bf2ae167d37ecc73e50443f3d1a0798b9
tkris618 <tkristian@my.wctc.edu>
2024-01-24 10:51:26 -0600

    [1.02] Chnaged button color

--- medals/src/components/Country.jsx
+++ medals/src/components/Country.jsx
@@ -25,7 +25,7 @@ class Country extends Component {
                 <Divider style={{marginBottom: '10px'}}/>
             <div className='medals' style={{fontSize: '1.5em', textTransform: 'capitalize', display: 'flex', alignItems: 'center', marginTop: '5px'}}>
                 <Avatar style={{backgroundColor: '#F4CBBA'}}>{ this.state.medals }</Avatar> <span style={{width: '130px', paddingLeft: '10px'}}>gold medals</span>
-                <Button variant="contained" onClick={this.handleClick}> <AddIcon></AddIcon> </Button>
+                <Button variant="contained" onClick={this.handleClick} style={{backgroundColor: '#C8A18F'}}> <AddIcon></AddIcon> </Button>
             </div>
             </div>
             </Paper>
